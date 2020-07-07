fb41afba7d0bbe1f5288b03fa3823312fffbe9de
khanmaster <eashkhan@hotmail.com>
2020-07-07 16:14:37 +0100

    API with requests moduel and excercise added update

--- README.md.py
+++ README.md.py
@@ -3,8 +3,10 @@
 # Exercise
 ## fetch data from API call using Requests module from https://api.postcodes.io/postcodes/se120nb
 ## display key:value pairs with in dictionaries
+## iterate through nested dictionaries and display outcome
 ## create a function to return the above values (key:value)
 ## create a class and move the code block inside the class
+
 
 
 # second exercise will be posted shortly
